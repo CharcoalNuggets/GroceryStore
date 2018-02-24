6e5087fb01a10388c1a7889d270d5da840c1585c
CharcoalNuggets <dgin@mail.csuchico.edu>
2018-02-23 16:20:23 -0800

    Agrocery list manager

--- main.cpp
+++ main.cpp
@@ -6,36 +6,36 @@ using namespace std;
 
 int main()
 {
-  string something;
-  string list[5]; //array of 5 strings
-  int numItems = 0;
-  char input;
-  do 
-  {
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
-  
-  if (input == 'A' || input == 'a')
-  {
-   cout<<"What is the item?\n";
-   cin>> something;
-  
-   if (numItems < 4)
-   {
-   list[numItems] = something;
-   numItems++;
-   }
+        string something;
+        string list[5]; //array of 5 strings
+        int numItems = 0;
+        char input;
+        do 
+        {
+                cout<<"\n==GROCERY LIST MANAGER==";
+                cout<<"\nEnter your choice: ";
+                cout<<"\n (A)dd an item";
+                cout<<"\n (Q)uit";
+                cout<<"\nYour choice (A/Q): ";
+                cin>>input;
 
-   else 
-   {
-   cout<<"You'll need a bigger list!\n";
-   }
-  }
- }while (!(input == 'Q' || input == 'q'));
-  
-  return 0;
+                if (input == 'A' || input == 'a')
+                {
+                        cout<<"What is the item?\n";
+                        cin>> something;
+
+                        if (numItems <= 4)
+                        {
+                                list[numItems] = something;
+                                numItems++;
+                        }
+
+                        else 
+                        {
+                                cout<<"You'll need a bigger list!\n";
+                        }
+                }
+        }while (!(input == 'Q' || input == 'q'));
+
+        return 0;
 }
